a251ed128fdfd5a49621e81f6e52e5f513ce1c70
Kanczs <kelvin.caya@ucsp.edu.pe>
2022-09-06 01:55:28 -0500

    2

---
+++ c++/2.20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int Pi = 3.1415;
+    int Radio{};
+
+    cout << "Ponga valor del radio:"; cin >> Radio;
+    cout << "El valor del diametro es"; cout << Radio * Radio << endl; 
+    cout << "La circunferencia es"; cout << 2*Pi*Radio << endl;
+    cout << "El area total es"; cout << Pi * (Radio * Radio) << endl;
+
+    return 0;
+}
---
+++ c++/2.26.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout <<"Muchas lineas: " << endl;
+
+    cout <<"* * * * " << endl;
+    cout <<" * * * * " << endl;
+    cout <<"* * * * " <<endl;
+    cout <<" * * * * " <<endl;
+    cout <<"* * * * " << endl;
+    cout <<" * * * * " << endl;
+
+    cout <<"Menos lineas: " << endl;
+    cout <<"* * * * * \n * * * * \n * * * * \n * * * * \n * * * * \n * * * * \n * * * * \n * * * * *\n" << endl;
+}
