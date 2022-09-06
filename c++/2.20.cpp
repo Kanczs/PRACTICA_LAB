760a2c27192cc6975377eec21356bbf2d9bad1ae
Kanczs <101739808+Kanczs@users.noreply.github.com>
2022-09-06 02:01:46 -0500

    Update 2.20.cpp

--- c++/2.20.cpp
+++ c++/2.20.cpp
@@ -7,9 +7,9 @@ int main(){
     int Radio{};
 
     cout << "Ponga valor del radio:"; cin >> Radio;
-    cout << "El valor del diametro es"; cout << Radio * Radio << endl; 
-    cout << "La circunferencia es"; cout << 2*Pi*Radio << endl;
-    cout << "El area total es"; cout << Pi * (Radio * Radio) << endl;
+    cout << "El valor del diametro es:"; cout << Radio * Radio << endl; 
+    cout << "La circunferencia es:"; cout << 2*Pi*Radio << endl;
+    cout << "El area total es:"; cout << Pi * (Radio * Radio) << endl;
 
     return 0;
 }
